e02edb10394b4d9bf57c8714c9f10e5000dd63cc
Shyam Saladi <saladi@caltech.edu>
2017-04-04 00:14:06 -0700

    Add regression testing (against codonR results)

---
+++ test/test_regression.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+import pandas as pd
+import numpy as np
+import Bio.SeqIO
+
+import tAI
+
+def check_against_ref(fna_file, ref_csv):
+    # Read reference features
+    calc = pd.read_csv(ref_csv, header=0, names=['old'], squeeze=False)
+
+    with open(fna_file, 'r+') as fh:
+        calc['new'] = pd.Series([tAI.calc_tAI(str(rec.seq)) \
+                                    for rec in Bio.SeqIO.parse(fh, 'fasta')])
+
+    assert np.allclose(calc['old'].values, calc['new'].values, atol=0.01)
+
+    return
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def test_ecolik12():
+    basename = os.path.join(dir_path, "data/ecolik12.ffn")
+    check_against_ref(basename, basename + ".tAI")
+    return
+
+
+def test_daley_gfp():
+    basename = os.path.join(dir_path, "data/Daley_gfp.fna")
+    check_against_ref(basename, basename + ".tAI")
+    return
+
+test_ecolik12()
